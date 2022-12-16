03761bc194389290a08ef687f1ace98d442afc85
renatogirao <renato.sgirao@gmail.com>
2022-12-16 18:33:00 -0300

    Criando table view

--- MovieDBProject/MovieListViewController.swift
+++ MovieDBProject/MovieListViewController.swift
@@ -8,14 +8,59 @@
 import UIKit
 
 class MovieListViewController: UIViewController {
-
+    
     var viewModel = MoviewViewModel()
+    
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView(frame: .zero, style: .insetGrouped)
+        tableView.separatorStyle = .none
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        return tableView
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .blue
         viewModel.fetchMovie()
-        
+        addSubviews()
+        setupConstraints()
+    }
+    
+    private func addSubviews() {
+        view.addSubview(tableView)
+    }
+    
+    private func setupConstraints() {
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.topAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            
+        ])
     }
 }
 
+extension MovieListViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        let title = "Filmes"
+        return title
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .default, reuseIdentifier: "UITableViewCell")
+        cell.textLabel?.text = "Celula \(indexPath.section), \(indexPath.item)"
+        return cell
+    }
+}
+
+extension MovieListViewController: UITableViewDelegate {
+    
+}
